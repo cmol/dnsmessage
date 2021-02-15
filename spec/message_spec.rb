a0079627f8c3cdcafa93e9f5f8e274cd5f3e566d
Claus Lensbøl <cmol@cmol.dk>
2021-02-15 11:40:03 -0500

    Use before statement

--- spec/message_spec.rb
+++ spec/message_spec.rb
@@ -1,17 +1,20 @@
 RSpec.describe DNSMessage::Message do
-  good_query =
-    "\x14\x4e\x01\x00\x00\x01\x00\x00\x00\x00\x00\x01\x04\x63\x6d\x6f" \
-    "\x6c\x02\x64\x6b\x00\x00\x01\x00\x01\x00\x00\x29\x02\x00\x00\x00" \
-    "\x00\x00\x00\x00"
-  good_reply =
-    "\x14\x4e\x81\x80\x00\x01\x00\x01\x00\x00\x00\x01\x04\x63\x6d\x6f" \
-    "\x6c\x02\x64\x6b\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00" \
-    "\x00\x1c\x20\x00\x04\x5d\x5a\x72\x37\x00\x00\x29\x10\x00\x00\x00" \
-    "\x00\x00\x00\x00"
+
+  before(:each) do
+    @good_query =
+      "\x14\x4e\x01\x00\x00\x01\x00\x00\x00\x00\x00\x01\x04\x63\x6d\x6f" \
+      "\x6c\x02\x64\x6b\x00\x00\x01\x00\x01\x00\x00\x29\x02\x00\x00\x00" \
+      "\x00\x00\x00\x00"
+    @good_reply =
+      "\x14\x4e\x81\x80\x00\x01\x00\x01\x00\x00\x00\x01\x04\x63\x6d\x6f" \
+      "\x6c\x02\x64\x6b\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\x00" \
+      "\x00\x1c\x20\x00\x04\x5d\x5a\x72\x37\x00\x00\x29\x10\x00\x00\x00" \
+      "\x00\x00\x00\x00"
+  end
 
   it "will parse DNS query header" do
     q = DNSMessage::Message.new()
-    q.parse_header(good_query[0...12])
+    q.parse_header(@good_query[0...12])
     expect(q).to have_attributes(qr: DNSMessage::Message::QUERY,
                                  rd: 1,
                                  qdcount: 1,
@@ -21,7 +24,7 @@ RSpec.describe DNSMessage::Message do
   end
 
   it "will parse DNS query" do
-    q = DNSMessage::Message.parse(good_query)
+    q = DNSMessage::Message.parse(@good_query)
     expect(q).to have_attributes(qr: DNSMessage::Message::QUERY,
                                  qdcount: 1,
                                  arcount: 1,
@@ -36,16 +39,16 @@ RSpec.describe DNSMessage::Message do
   end
 
   it "will fail on too short query" do
-    expect {DNSMessage::Message.parse(good_query[0..10])}.to raise_error(StandardError)
+    expect {DNSMessage::Message.parse(@good_query[0..10])}.to raise_error(StandardError)
   end
   it "will fail on reply type" do
-    bad_query = good_query
+    bad_query = @good_query
     bad_query[2] = [0x80].pack("c")
     expect {DNSMessage::Message.parse(bad_query).check_validity}
       .to raise_error(StandardError)
   end
   it "will fail on no questions in query" do
-    bad_query = good_query
+    bad_query = @good_query
     bad_query[4] = [0x0].pack("c")
     bad_query[5] = [0x0].pack("c")
     expect {DNSMessage::Message.parse(bad_query).check_validity}
@@ -55,7 +58,7 @@ RSpec.describe DNSMessage::Message do
   it "will parse DNS record" do
     q  = DNSMessage::Message.new()
     name_ptrs = DNSMessage::Pointer.new({0x0c => "cmol.dk"})
-    expect(q.parse_records(good_reply, 1, 25, name_ptrs)[0][0]).to \
+    expect(q.parse_records(@good_reply, 1, 25, name_ptrs)[0][0]).to \
       have_attributes(
              name: "cmol.dk",
              type: 7200,
@@ -66,7 +69,7 @@ RSpec.describe DNSMessage::Message do
   end
 
   it "will parse DNS reply" do
-    q = DNSMessage::Message.parse(good_reply)
+    q = DNSMessage::Message.parse(@good_reply)
     expect(q).to have_attributes(qr: DNSMessage::Message::REPLY,
                                  qdcount: 1,
                                  arcount: 1,
@@ -90,7 +93,7 @@ RSpec.describe DNSMessage::Message do
   end
 
   it "will build reply correctly" do
-    r = DNSMessage::Message::parse(good_reply).build
-    expect(r.bytes).to eq(good_reply.bytes)
+    r = DNSMessage::Message::parse(@good_reply).build
+    expect(r.bytes).to eq(@good_reply.bytes)
   end
 end
